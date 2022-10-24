0a621dbbaf212cda21dab0ab1d95672533867214
nate3211 <116321525+nate3211@users.noreply.github.com>
2022-10-24 11:47:13 -0400

    Update in_class_assignment_5.py

--- in_class_assignment_5.py
+++ in_class_assignment_5.py
@@ -4,6 +4,23 @@
 # The main script should return the finished sorted list as "sorted.txt"
 # All 3 files "In_class_assignment_5.py", "numbers.txt", and "sorted.txt" should all be added to your github repository and submitted as a github link.
 
+'''Info on Quicksort Algorithm: 
+The Quicksort algorithm is an efficient sorting algorithm developed by British 
+computer scientist Tony Hoare in 1959.
+Quicksort is a divide-and-conquer algorithm. Suppose you have a list of objects to 
+sort. You start by choosing an item in the list, called the *pivot item*. 
+This can be any item in the list. You then partition the list into two sublists 
+based on the pivot item and recursively sort the sublists.
+The steps of the algorithm are as follows:
+1. Choose the pivot item.
+2. Partition the list into two sublists:
+        Those items that are less than the pivot item
+        Those items that are greater than the pivot item
+3. Quicksort the sublists recursively.
+4. Each partitioning produces smaller sublists, so the algorithm is reductive. 
+The base cases occur when the sublists are either empty or have one element, as 
+these are inherently sorted. 
+ '''
 import re
 # Quicksort function 
 def quicksort(numbers_in_a_list):
